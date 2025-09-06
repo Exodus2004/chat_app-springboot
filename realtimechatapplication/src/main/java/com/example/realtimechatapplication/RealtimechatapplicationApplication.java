20b7b722f4a075997d63b9c4a0cd35493cd07d00
exodus2004 <flyingskyinthehouse@gmail.com>
2025-09-06 17:12:12 +0530

    Fix build and add comment

--- realtimechatapplication/src/main/java/com/example/realtimechatapplication/RealtimechatapplicationApplication.java
+++ realtimechatapplication/src/main/java/com/example/realtimechatapplication/RealtimechatapplicationApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RealtimechatapplicationApplication {
+	//hello world
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealtimechatapplicationApplication.class, args);
